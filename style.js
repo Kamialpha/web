cbce3577f017fb2841db6a3d4a185c04a88d05e0
Kamialpha <arashbana0098@gmail.com>
2022-01-04 02:24:27 -0800

    Create style.js

---
+++ style.js
@@ -0,0 +1,83 @@
+// var demo = 2;
+// switch ( demo ){
+//     case 1:
+//          console.log(" shanbe ");
+//     break;
+//     case 2:
+//         console.log(" 1 shanbe ");
+//     break;
+//     case 3:
+//         console.log(" 2 shanbe ");
+//     break;
+//     case 4:
+//         console.log(" 3 shanbe ");
+//     break;
+//     case 5:
+//         console.log(" 4 shanbe ");
+//     break;
+//     case 6:
+//         console.log(" 5 shanbe ");
+//     break;
+//     case 7:
+//         console.log(" 6 shanbe ");
+//     break;
+//     default:
+//         console.log(" na motabar ");
+//     break;
+// }
+// var kami = 49;
+// if (kami <= 10){
+//     console.log("age > 10");
+// }
+// else if (kami <= 20){
+//     console.log(" age < 20");
+// }
+// else if (kami <= 30){
+//     console.log(" age < 30");
+// }
+// else if (kami <= 40){
+//     console.log(" age < 40");
+// }
+// else if (kami <= 50){
+//     console.log(" age < 50");
+// }
+// else if (kami <= 60){
+//     cosole.log(" age < 60");
+// }
+// else if (kami <= 70){
+//     console.log(" age < 70");
+//     alert("is age = < 70");
+// }
+// else if (kami <= 80){
+//     console.log(" age < 80");
+// }
+// else if (kami <= 90){
+//     console.log(" age < 90");
+// }
+// else if(kami <= 100){
+//     console.log(" age < 100");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
